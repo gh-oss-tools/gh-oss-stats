3c872ee0784e8a7eb556adaeade4da13e3ce7f7e
MABD <mabd.universe@gmail.com>
2025-12-31 07:26:00 +0200

    updated code comments

--- cmd/gh-oss-stats/main.go
+++ cmd/gh-oss-stats/main.go
@@ -8,16 +8,14 @@ import (
 const version = "0.3.1"
 
 func main() {
-	// Get arguments after program name
 	args := os.Args[1:]
 
-	// If no arguments or first arg is not a sub-command, run main command
 	if len(args) == 0 {
 		runMainCmd(args)
 		return
 	}
 
-	// Route to sub-commands
+	// Route to sub-commands, or fallback to main command
 	switch args[0] {
 	case "badge":
 		runBadgeCmd(args[1:])
@@ -27,7 +25,6 @@ func main() {
 		fmt.Printf("gh-oss-stats v%s\n", version)
 		os.Exit(0)
 	default:
-		// If first arg doesn't match a sub-command, treat as main command args
 		runMainCmd(args)
 	}
 }
